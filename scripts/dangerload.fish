7b923b412296568c98ba4470116249f3c7a2ae03
redaphid <iam@aaronherres.com>
2021-09-23 20:21:21 -0700

    export by id + convert to web-ready svg

--- scripts/dangerload.fish
+++ scripts/dangerload.fish
@@ -2,4 +2,6 @@
  function svg-export-id -a file -a id
     set tmp_file tmp/$id-inkscape.svg
     inkscape --export-type="svg" --export-id="$id" --export-id-only --export-filename="$tmp_file" $file
+    scour --strip-xml-prolog -i $tmp_file -o images/$id.svg
+    # rm $tmp_file
  end
